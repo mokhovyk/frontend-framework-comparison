9e6a7757ffe2eee49e4b1bea516f4e628df7eba9
Andrii Mokhovyk <andriimo@wix.com>
2026-04-06 16:54:26 +0300

    fix svelte build

--- frameworks/vue/vite.config.ts
+++ frameworks/vue/vite.config.ts
@@ -2,12 +2,23 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
+const app = process.env.APP || 'table';
+
 export default defineConfig({
   plugins: [vue()],
+  root: resolve(__dirname, `apps/${app}`),
+  build: {
+    outDir: resolve(__dirname, `dist/${app}`),
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       'shared-data': resolve(__dirname, '../../packages/shared-data/src'),
       'shared-css': resolve(__dirname, '../../packages/shared-css'),
     },
+  },
+  server: {
+    port: 3000,
+    strictPort: false,
   },
 });
